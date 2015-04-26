2fdba531a135e8afdb80aefa031c62e49787c490
sinsoku <sinsoku.listy@gmail.com>
2015-04-26 17:35:34 +0900

    Update ruby 2.2 syntax

--- lib/active_admin/mongoid/helpers/collection.rb
+++ lib/active_admin/mongoid/helpers/collection.rb
@@ -4,7 +4,7 @@ module ActiveAdmin
 
       alias original_collection_size collection_size
       original_collection_size = instance_method(:collection_size)
-      def collection_size(collection=collection)
+      def collection_size(collection=collection())
         if(not collection.empty? and collection.first.class.included_modules.include?(Mongoid::Document))
           if collection.first.class.embedded?
             collection.count
